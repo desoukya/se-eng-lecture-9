14fcfb15046618ef0f0e49871ddbab6b8d8866ef
Amr Desouky <desoukya@gmail.com>
2022-12-14 06:47:04 -0600

    add null guard if cookie header is not defined

--- utils/session.js
+++ utils/session.js
@@ -1,5 +1,8 @@
 module.exports = {
   getSessionToken(req) {
+    if (!req.headers.cookie) {
+      return null;
+    }
     const cookies = req.headers.cookie.split(';')
       .map(function (cookie) { return cookie.trim() })
       .filter(function (cookie) { return cookie.includes('session_token') })
